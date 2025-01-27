26cbcac8ad6e5eba79598763f16a54f9cf0479c8
Bruno Fernandes <synystry1989@gmail.com>
2025-01-27 19:17:57 +0000

    automatic_install_DB

--- Program.cs
+++ Program.cs
@@ -36,7 +36,8 @@ namespace TeleBerço
             // Caminho do script SQL na pasta Resources
             string scriptPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Resources", "Gestao&Stock_TB2017.sql");
             string nomeBanco = "DB_TeleBerco2017";
-            // Verificar se o arquivo existe
+
+            // Verificar se o arquivo de script existe
             if (!File.Exists(scriptPath))
             {
                 throw new FileNotFoundException("O arquivo Gestao&Stock_TB2017.sql não foi encontrado na pasta Resources.");
@@ -47,87 +48,107 @@ namespace TeleBerço
                 // String de conexão ao banco 'master' para criação do banco de dados
                 string masterConnectionString = $"Server={nomeServidor};Database=master;Integrated Security=True;Encrypt=True;TrustServerCertificate=True;";
 
-                // Ler o script SQL
-                string script = File.ReadAllText(scriptPath);
-
-                // Dividir o script com base no "GO"
-                string[] commands = script.Split(new[] { "GO" }, StringSplitOptions.RemoveEmptyEntries);
-
-                using (SqlConnection connection = new SqlConnection(masterConnectionString))
+                // Verificar se o banco de dados já existe
+                if (!BancoDeDadosExiste(nomeServidor, nomeBanco))
                 {
-                    connection.Open();
+                    // Ler o script SQL para criar o banco
+                    string script = File.ReadAllText(scriptPath);
 
-                    foreach (var command in commands)
+                    // Dividir o script com base no "GO"
+                    string[] commands = script.Split(new[] { "GO" }, StringSplitOptions.RemoveEmptyEntries);
+
+                    using (SqlConnection connection = new SqlConnection(masterConnectionString))
                     {
-                        if (!string.IsNullOrWhiteSpace(command))
+                        connection.Open();
+
+                        foreach (var command in commands)
                         {
-                            using (SqlCommand sqlCommand = new SqlCommand(command, connection))
+                            if (!string.IsNullOrWhiteSpace(command))
                             {
-                                sqlCommand.ExecuteNonQuery();
+                                using (SqlCommand sqlCommand = new SqlCommand(command, connection))
+                                {
+                                    sqlCommand.ExecuteNonQuery();
+                                }
                             }
                         }
                     }
+
+                    MessageBox.Show($"Banco de dados {nomeBanco} configurado com sucesso!");
+
+                    // Atualizar a string de conexão para o banco recém-criado
+                    AtualizarStringConexao(nomeServidor, nomeBanco);
                 }
+                
 
-                Console.WriteLine("Banco de dados configurado com sucesso!");
+                // Verificar se a tabela TipoDocumentos está vazia e inserir os dados, se necessário
+                InserirDadosSeNecessario(nomeServidor, nomeBanco);
 
-
-                // Atualizar a string de conexão para o banco recém-criado
-                AtualizarStringConexao(nomeServidor,nomeBanco);
-
-                string scriptDados = @"
-            USE DB_Teleberco2017;
-
-            -- Criar a tabela TipoDocumentos
-            IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'TipoDocumentos')
-            BEGIN
-                CREATE TABLE TipoDocumentos (
-                    Id INT IDENTITY(1,1) PRIMARY KEY,
-                    Nome NVARCHAR(50) NOT NULL,
-                    Descricao NVARCHAR(255) NOT NULL
-                );
-            END;
-
-            -- Inserir os valores na tabela TipoDocumentos
-            INSERT INTO TipoDocumentos (Nome, Descricao)
-            VALUES 
-                ('FTC', 'Fatura Cliente'),
-                ('NDC', 'Nota Devolução Cliente'),
-                ('NDF', 'Nota Devolução Fornecedor'),
-                ('EDC', 'Encomenda de Cliente'),
-                ('EDF', 'Encomenda de Fornecedor'),
-                ('ORC', 'Orçamento');
-        ";
-
-
-                // String de conexão ao banco DB_Teleberco2017
-                string connectionString = $"Server={nomeServidor};Database={nomeBanco};Integrated Security=True;Encrypt=True;TrustServerCertificate=True;";
-
-                using (SqlConnection connection = new SqlConnection(connectionString))
-                {
-                    connection.Open();
-                    using (SqlCommand command = new SqlCommand(scriptDados, connection))
-                    {
-                        command.ExecuteNonQuery();
-                    }
-                }
-
-                Console.WriteLine("Tabela TipoDocumentos criada e registros inseridos com sucesso!");
             }
-
-
             catch (Exception ex)
             {
-                Console.WriteLine($"Erro ao configurar o banco de dados: {ex.Message}");
+                MessageBox.Show($"Erro ao configurar o banco de dados: {ex.Message}");
                 throw;
+            }
+        }
+
+        private static bool BancoDeDadosExiste(string nomeServidor, string nomeBanco)
+        {
+            string connectionString = $"Server={nomeServidor};Database=master;Integrated Security=True;Encrypt=True;TrustServerCertificate=True;";
+            string query = $"SELECT database_id FROM sys.databases WHERE name = '{nomeBanco}'";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    var result = command.ExecuteScalar();
+                    return result != null;
+                }
+            }
+        }
+
+        private static void InserirDadosSeNecessario(string nomeServidor, string nomeBanco)
+        {
+            // String de conexão ao banco DB_Teleberco2017
+            string connectionString = $"Server={nomeServidor};Database={nomeBanco};Integrated Security=True;Encrypt=True;TrustServerCertificate=True;";
+
+            // Consultar se a tabela TipoDocumentos está vazia
+            string query = "SELECT COUNT(*) FROM TipoDocumentos";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    int count = (int)command.ExecuteScalar();
+                    if (count == 0)
+                    {
+                        // A tabela está vazia, inserir os dados
+                        string scriptDados = @"
+                    INSERT INTO TipoDocumentos (CodDoc, Descricao)
+                    VALUES 
+                        ('FTC', 'Fatura Cliente'),
+                        ('NDC', 'Nota Devolução Cliente'),
+                        ('NDF', 'Nota Devolução Fornecedor'),
+                        ('EDC', 'Encomenda de Cliente'),
+                        ('EDF', 'Encomenda de Fornecedor'),
+                        ('ORC', 'Orçamento');
+                ";
+
+                        using (SqlCommand insertCommand = new SqlCommand(scriptDados, connection))
+                        {
+                            insertCommand.ExecuteNonQuery();
+                        }
+
+                        MessageBox.Show("Registros inseridos com sucesso!");
+                    }
+                 
+                }
             }
         }
 
         private static void AtualizarStringConexao(string nomeServidor, string nomeBanco)
         {
-            // Nome fixo do banco de dados criado
-            
-
             // Nova string de conexão com o banco de dados criado
             string novaStringConexao = $"Server={nomeServidor};Database={nomeBanco};Integrated Security=True;Encrypt=True;TrustServerCertificate=True;";
 
@@ -156,7 +177,7 @@ namespace TeleBerço
                     // Salvar o arquivo de configuração atualizado
                     configDoc.Save(configPath);
 
-                    Console.WriteLine("String de conexão atualizada com sucesso!");
+                    MessageBox.Show("String de conexão atualizada com sucesso!");
                 }
                 else
                 {
@@ -165,9 +186,11 @@ namespace TeleBerço
             }
             catch (Exception ex)
             {
-                Console.WriteLine($"Erro ao atualizar a string de conexão: {ex.Message}");
+                MessageBox.Show($"Erro ao atualizar a string de conexão: {ex.Message}");
                 throw;
             }
         }
+ 
+
     }
 }
